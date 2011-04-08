ad80950393c60a5d243c4bb9eb5044b394d44390
mgrcar <mgrcar@35089d11-96e9-ae4d-82a0-4b2588c93174>
2011-04-08 19:32:53 +0000

    Small fixes
    git-svn-id: svn://first.ijs.si:3691/DacqPipe@165 35089d11-96e9-ae4d-82a0-4b2588c93174

--- RssScraper/RssScraper/RssScraper.cs
+++ RssScraper/RssScraper/RssScraper.cs
@@ -1,9 +1,9 @@
 using System;
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
+using System.Threading;
 using Latino;
 using Latino.Web;
-using System.Threading;
 
 namespace RssScraper
 {
