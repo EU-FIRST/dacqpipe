8411c4af3fa32b4832e2fd57777f2207d0039a27
mgrcar <mgrcar@35089d11-96e9-ae4d-82a0-4b2588c93174>
2012-07-25 19:34:12 +0000

    Changed SQL statement in GetDocRefs
    git-svn-id: svn://first.ijs.si:3691/DacqPipe@666 35089d11-96e9-ae4d-82a0-4b2588c93174

--- FIRSTDatasetService/App_Code/Service.cs
+++ FIRSTDatasetService/App_Code/Service.cs
@@ -22,11 +22,14 @@ public class Service : WebService
     [WebMethod]
     public string[][] GetDocRefs(string sourceUrl)
     {
-        string selectStatement = string.Format(@"select c.timeEnd, d.corpusId, d.id from Corpora c join Documents d on c.id = d.corpusId 
+        string selectStatement = @"select timeEnd, corpusId, id from
+            (select *, row_number() over (partition by urlKey order by time desc, rev desc) rn from
+            (select d.urlKey, d.rev, d.corpusId, d.id, d.time, c.timeEnd from Corpora c join Documents d on c.id = d.corpusId 
             where sourceUrl like ? and c.rejected = 0 and d.rejected = 0
             union
-            select c.timeEnd, d.corpusId, d.id from Sources s join Corpora c on s.siteId = c.siteId join Documents d on s.docId = d.id and c.id = d.corpusId
-            where s.sourceUrl like ? and d.rejected = 0 and c.rejected = 0");
+            select d.urlKey, d.rev, d.corpusId, d.id, d.time, c.timeEnd from Sources s join Corpora c on s.siteId = c.siteId join Documents d on s.docId = d.id and c.id = d.corpusId
+            where s.sourceUrl like ? and c.rejected = 0 and d.rejected = 0) as a) as b
+            where rn = 1";
         DatabaseConnection dbConnection = new DatabaseConnection();
         dbConnection.ConnectionString = Utils.GetConfigValue("DbConnectionString", 
             "Provider=SQLNCLI10;Server=(local);Database=DacqPipe;Trusted_Connection=Yes");
@@ -51,16 +54,19 @@ public class Service : WebService
         if (docId == null || docId.Replace("-", "").Length != 32) { return "*** Invalid document ID."; }
         docId = docId.Replace("-", "");
         string[] fileNames = null;
-        try
+        if (!string.IsNullOrEmpty(time))
         {
-            DateTime dt = DateTime.Parse(time);
-            string prefix = dt.ToString("HH_mm_ss_");
-            string path = "\\" + dt.Year + "\\" + dt.Month + "\\" + dt.Day + "\\";
-            string fileName = dataPath.TrimEnd('\\') + path + prefix + corpusId + ".xml";
-            if (!Utils.VerifyFileNameOpen(fileName)) { return "*** Corpus not found."; }
-            fileNames = new string[] { fileName };
+            try
+            {
+                DateTime dt = DateTime.Parse(time);
+                string prefix = dt.ToString("HH_mm_ss_");
+                string path = "\\" + dt.Year + "\\" + dt.Month + "\\" + dt.Day + "\\";
+                string fileName = dataPath.TrimEnd('\\') + path + prefix + corpusId + ".xml";
+                if (!Utils.VerifyFileNameOpen(fileName)) { return "*** Corpus not found."; }
+                fileNames = new string[] { fileName };
+            }
+            catch { return "*** Unable to parse time."; }
         }
-        catch { }
         if (fileNames == null) { fileNames = Directory.GetFiles(dataPath, "*" + corpusId + ".xml", SearchOption.AllDirectories); }
         if (fileNames.Length == 0) { return "*** Corpus not found."; }
         DocumentCorpus corpus = new DocumentCorpus();
