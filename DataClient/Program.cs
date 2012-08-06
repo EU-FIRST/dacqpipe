8c76e20e3ef2d5c3dabc424901b654c68bbe926e
mgrcar <mgrcar@35089d11-96e9-ae4d-82a0-4b2588c93174>
2012-08-06 06:33:41 +0000

    Caching mechanism revised
    git-svn-id: svn://first.ijs.si:3691/DacqPipe@687 35089d11-96e9-ae4d-82a0-4b2588c93174

--- DataClient/Program.cs
+++ DataClient/Program.cs
@@ -18,9 +18,26 @@ namespace DataClient
             string line;
             while ((line = reader.ReadLine()) != null)
             {
-                mCache.Add(line);
+                string[] lineParsed = line.Split('\t');
+                mCache.AddRange(GenerateCacheKeys(lineParsed[0], lineParsed[1], lineParsed[2]));
             }
             reader.Close();
+        }
+
+        static ArrayList<string> GenerateCacheKeys(string corpusId, string docId, string lbl)
+        {
+            ArrayList<string> keys = new ArrayList<string>();
+            string[] lblParsed = lbl.Split('/');
+            string partLbl = lblParsed[0];
+            keys.Add(corpusId + "\t" + docId + "\t" + partLbl);
+            //Console.WriteLine("caching " + partLbl);
+            for (int i = 1; i < lblParsed.Length; i++)
+            {
+                partLbl += '/' + lblParsed[i];
+                keys.Add(corpusId + "\t" + docId + "\t" + partLbl);
+                //Console.WriteLine("caching " + partLbl);
+            }
+            return keys;
         }
 
         static void RetrieveDocuments(string sourceUrl, string lbl, string corpusFileOut, string cacheFileOut, string timeStart, string timeEnd)
@@ -52,7 +69,7 @@ namespace DataClient
                             corpus.Flush();
                             cache.WriteLine(cacheKey);
                             cache.Flush();
-                            mCache.Add(cacheKey);
+                            mCache.AddRange(GenerateCacheKeys(corpusId, docId, lbl));
                         }
                         else
                         {
@@ -100,11 +117,9 @@ namespace DataClient
                 {
                     Debug.Assert(!ln.Contains("/"));
                     UpdateLbl(line, lblArr);
-                    //Console.WriteLine(lblArr);
                 }
                 else
                 {
-                    //Console.WriteLine("URL " + ln);
                     string lblFull = "";
                     foreach (string lbl in lblArr)
                     {
@@ -118,22 +133,30 @@ namespace DataClient
 
         static void GroundTaxonomy(ArrayList<Pair<string, string>> tax, string corpusFileOut, string cacheFileOut, string timeStart, string timeEnd)
         {
+            ArrayList<KeyDat<int, Pair<string, string>>> tmp = new ArrayList<KeyDat<int, Pair<string, string>>>();
             foreach (Pair<string, string> item in tax)
             {
-                Console.WriteLine("Grounding \"{0}\" with {1} ...", item.First, item.Second);
-                RetrieveDocuments(item.Second, item.First, corpusFileOut, cacheFileOut, timeStart, timeEnd);
+                int lvl = 1;
+                foreach (char ch in item.First) { if (ch == '/') { lvl++; } }
+                tmp.Add(new KeyDat<int, Pair<string, string>>(lvl, item));
+            }
+            tmp.Sort(DescSort<KeyDat<int, Pair<string, string>>>.Instance);
+            foreach (KeyDat<int, Pair<string, string>> item in tmp)
+            {
+                Console.WriteLine("Grounding \"{0}\" with {1} ...", item.Dat.First, item.Dat.Second);
+                RetrieveDocuments(item.Dat.Second, item.Dat.First, corpusFileOut, cacheFileOut, timeStart, timeEnd);
             }
             Console.WriteLine("Grounding done.");
         }
 
         static void Main(string[] args)
         {
-            LoadFromCache(@"C:\Work\DacqPipe\DataClient\YahooRssTx_cache.txt");
-            ArrayList<Pair<string, string>> tax = LoadTaxonomy(@"C:\Work\DacqPipe\DataClient\YahooNewsCategories.txt");
+            //LoadFromCache(@"C:\Work\DacqPipe\DataClient\YahooRssTx_cache_2.txt");
+            ArrayList<Pair<string, string>> tax = LoadTaxonomy(@"C:\Work\DacqPipe\DataClient\YahooNewsCategories_2.txt");
             GroundTaxonomy(tax,
-                @"C:\Work\DacqPipe\DataClient\YahooRssTx.txt", 
-                @"C:\Work\DacqPipe\DataClient\YahooRssTx_cache.txt", 
-                "2012-07-01", "2012-07-05");
+                @"C:\Work\DacqPipe\DataClient\YahooRssTx_2.txt", 
+                @"C:\Work\DacqPipe\DataClient\YahooRssTx_cache_2.txt", 
+                "2012-06-01", "2012-08-01");
             //LoadFromCache(@"C:\Work\DacqPipe\DataClient\YahooFinance_cache.txt");
             //RetrieveDocuments("http://finance.yahoo.com/rss/%", 
             //    "YahooFinance",
